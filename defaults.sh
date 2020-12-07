b22137a16ca3f3c9d19a2c821a7583581e31fc3d
Spencer Hachmeister <tms@spencerslab.com>
2020-12-06 17:14:46 -0700

    Reenable Prometheus and tweak config to work with latest TechnoCore.

--- defaults.sh
+++ defaults.sh
@@ -5,6 +5,6 @@ set_service_flag prometheus
 #prefix=
 
 # If the include exporters flag is set
-if [ ! -z "$SERVICE_prometheus_exporters" ]; then
-    export SERVICE_CONFIG_prometheus_exporters=${TECHNOCORE_SERVICES}/prometheus/exporters.yml
+if [ ! -z "$SERVICE_PROMETHEUS_EXPORTERS" ]; then
+    export SERVICE_CONFIG_PROMETHEUS_EXPORTERS=${TECHNOCORE_SERVICES}/prometheus/exporters.yml
 fi
